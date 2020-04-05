c7a39c3d2f4d1ad96e682d3284b80024a4a983da
frozenparadox99 <frostbitecryengine300@gmail.com>
2020-04-05 23:21:38 +0530

    created a modal for adding channels

--- src/components/SidePanel/UserPanel.js
+++ src/components/SidePanel/UserPanel.js
@@ -1,14 +1,20 @@
 import React, { Component } from "react";
-import { Grid, Header, Icon, Dropdown } from "semantic-ui-react";
+import { Grid, Header, Icon, Dropdown, Image } from "semantic-ui-react";
+import firebase from "../../firebase";
+import AvatarEditor from "react-avatar-editor";
 
 class UserPanel extends Component {
+  state = {
+    user: this.props.currentUser,
+  };
+
   dropdownOptions = () => {
     return [
       {
         key: "user",
         text: (
           <span>
-            Signed in as <strong>User</strong>
+            Signed in as <strong>{this.state.user.displayName}</strong>
           </span>
         ),
         disabled: true,
@@ -19,12 +25,20 @@ class UserPanel extends Component {
       },
       {
         key: "signout",
-        text: <span>Sign Out</span>,
+        text: <span onClick={this.handleSignout}>Sign Out</span>,
       },
     ];
   };
 
+  handleSignout = () => {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => console.log("signed out!"));
+  };
+
   render() {
+    const { user } = this.state;
     return (
       <Grid style={{ background: "#4c3c4c" }}>
         <Grid.Column>
@@ -33,14 +47,19 @@ class UserPanel extends Component {
               <Icon name="code" />
               <Header.Content>DevChat</Header.Content>
             </Header>
-          </Grid.Row>
 
-          <Header style={{ padding: "0.25em" }} as="h4" inverted>
-            <Dropdown
-              trigger={<span>User</span>}
-              options={this.dropdownOptions()}
-            />
-          </Header>
+            <Header style={{ padding: "0.25em" }} as="h4" inverted>
+              <Dropdown
+                trigger={
+                  <span>
+                    <Image src={user.photoURL} spaced="right" avatar />
+                    {user.displayName}
+                  </span>
+                }
+                options={this.dropdownOptions()}
+              />
+            </Header>
+          </Grid.Row>
         </Grid.Column>
       </Grid>
     );
