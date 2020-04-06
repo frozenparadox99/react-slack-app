46154f626e95272edaf1c480ae3351a18b393c55
frozenparadox99 <frostbitecryengine300@gmail.com>
2020-04-07 01:42:55 +0530

    created messages component and added the display messages functionality

--- src/components/Messages/MessageForm.js
+++ src/components/Messages/MessageForm.js
@@ -63,13 +63,14 @@ class MessageForm extends Component {
   };
 
   render() {
-    const { errors } = this.state;
+    const { errors, message, loading } = this.state;
     return (
       <Segment className="message__form">
         <Input
           fluid
           name="message"
           onChange={this.handleChange}
+          value={message}
           style={{ marginBottom: "0.7em" }}
           label={<Button icon={"add"} />}
           labelPosition="left"
@@ -83,6 +84,7 @@ class MessageForm extends Component {
         <Button.Group icon widths="2">
           <Button
             onClick={this.sendMessage}
+            disabled={loading}
             color="orange"
             content="Add Reply"
             labelPosition="left"
--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -3,21 +3,66 @@ import firebase from "../../firebase";
 import { Segment, Comment } from "semantic-ui-react";
 import MessagesHeader from "./MessagesHeader";
 import MessageForm from "./MessageForm";
+import Message from "./Message";
 
 class Messages extends Component {
   state = {
     messagesRef: firebase.database().ref("messages"),
+    messages: [],
     channel: this.props.currentChannel,
     user: this.props.currentUser,
+    messagesLoading: true,
   };
+
+  componentDidMount() {
+    const { channel, user } = this.state;
+
+    if (channel && user) {
+      this.addListeners(channel.id);
+    }
+  }
+
+  addListeners = (channelId) => {
+    this.addMessageListener(channelId);
+  };
+
+  addMessageListener = (channelId) => {
+    let loadedMessages = [];
+    this.state.messagesRef.child(channelId).on("child_added", (snap) => {
+      loadedMessages.push(snap.val());
+      this.setState({
+        messages: loadedMessages,
+        messagesLoading: false,
+      });
+    });
+  };
+
+  displayMessages = (messages) =>
+    messages.length > 0 &&
+    messages.map((message) => (
+      <Message
+        key={message.timestamp}
+        message={message}
+        user={this.state.user}
+      />
+    ));
+
   render() {
-    const { messagesRef, channel, user } = this.state;
+    const {
+      messagesRef,
+      channel,
+      user,
+      messages,
+      messagesLoading,
+    } = this.state;
     return (
       <Fragment>
         <MessagesHeader />
 
         <Segment>
-          <Comment.Group className="messages"></Comment.Group>
+          <Comment.Group className="messages">
+            {this.displayMessages(messages)}
+          </Comment.Group>
         </Segment>
         <MessageForm
           messagesRef={messagesRef}
