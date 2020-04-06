42a41fa4e9c30b62ca816b7d5bddfcccc0f77eae
frozenparadox99 <frostbitecryengine300@gmail.com>
2020-04-06 23:44:04 +0530

    created message form and header component

--- src/components/ColorPanel/ColorPanel.js
+++ src/components/ColorPanel/ColorPanel.js
@@ -1,8 +1,30 @@
 import React, { Component } from "react";
+import {
+  Sidebar,
+  Menu,
+  Divider,
+  Button,
+  Modal,
+  Icon,
+  Label,
+  Segment,
+} from "semantic-ui-react";
 
 class ColorPanel extends Component {
   render() {
-    return <div>ColorPanel</div>;
+    return (
+      <Sidebar
+        as={Menu}
+        icon="labeled"
+        inverted
+        vertical
+        visible
+        width="very thin"
+      >
+        <Divider />
+        <Button icon="add" size="small" color="blue" />
+      </Sidebar>
+    );
   }
 }
 
