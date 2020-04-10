6800ec55f62856c2032bc0d31d1f71878320f373
frozenparadox99 <frostbitecryengine300@gmail.com>
2020-04-10 20:59:48 +0530

    added ColorPanel

--- src/components/App.js
+++ src/components/App.js
@@ -9,9 +9,19 @@ import MetaPanel from "./MetaPanel/MetaPanel";
 
 import "./App.css";
 
-const App = ({ currentUser, currentChannel, isPrivateChannel, userPosts }) => (
+const App = ({
+  currentUser,
+  currentChannel,
+  isPrivateChannel,
+  userPosts,
+  primaryColor,
+  secondaryColor,
+}) => (
   <Grid columns="equal" className="app" style={{ background: "#eee" }}>
-    <ColorPanel />
+    <ColorPanel
+      key={currentUser && currentUser.name}
+      currentUser={currentUser}
+    />
     <SidePanel key={currentUser && currentUser.id} currentUser={currentUser} />
     <Grid.Column style={{ marginLeft: 320 }}>
       <Messages
@@ -37,6 +47,8 @@ const mapStateToProps = (state) => ({
   currentChannel: state.channel.currentChannel,
   isPrivateChannel: state.channel.isPrivateChannel,
   userPosts: state.channel.userPosts,
+  primaryColor: state.colors.primaryColor,
+  secondaryColor: state.colors.secondaryColor,
 });
 
 export default connect(mapStateToProps)(App);
